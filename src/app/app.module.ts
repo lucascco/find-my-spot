27a2b31e3b655fabb2cf1255ace25524141c4d96
Lucas Carvalho <lucasccorrea@gmail.com>
2018-09-26 12:32:27 -0300

    finish page of payments

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,13 +13,15 @@ import { UserProvider } from '../providers/user/user';
 import { LoginPage } from '../pages/login/login';
 import { SignupPage } from '../pages/signup/signup';
 import { UtisProvider } from '../providers/utis/utis';
+import { PaymentsPage } from '../pages/payments/payments';
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
     LoginPage,
-    SignupPage
+    SignupPage,
+    PaymentsPage
   ],
   imports: [
     BrowserModule,
@@ -31,7 +33,8 @@ import { UtisProvider } from '../providers/utis/utis';
     MyApp,
     HomePage,
     LoginPage,
-    SignupPage
+    SignupPage,
+    PaymentsPage
   ],
   providers: [
     StatusBar,
